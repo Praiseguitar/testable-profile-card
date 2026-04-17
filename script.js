8095a0fdf3f59492fe56e7d4e7b012ff91efa683
Praise <oyekalepraisestephen@gmail.com>
2026-04-17 23:52:31 +0100

    Testable Profile Card

---
+++ script.js
@@ -0,0 +1,24 @@
+const timeEl = document.querySelector('[data-textid="test-user-time"]');
+
+function updateTime() {
+    timeEl.textContent = `Current Time: ${Date.now()}`
+}
+updateTime();
+
+setInterval(updateTime, 1000);
+
+const avatar = document.querySelector('[data-textid="test-user-avatar"]');
+const avatarUpload = document.getElementById("avartarUpload");
+
+avatarUpload.addEventListener("change", () => {
+    const file = avatarUpload.file[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            avatar.src = e.target.result
+        };
+        reader.readAsDataURL(file)
+    }
+});
